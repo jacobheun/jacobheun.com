49a590c11100cd7d3373ad5243dc97da93eca3a7
Jake Heun <jake.heun@meltmedia.com>
2018-01-21 14:18:17 -0700

    Add basic setup

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const stylus = require('gulp-stylus');
+const connect = require('gulp-connect');
+
+gulp.task('pug', function(){
+  return gulp.src('templates/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('stylus', function () {
+  return gulp.src('css/**/*.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('dev_server', function () {
+  connect.server({
+    name: 'Dev server',
+    root: ['build'],
+    port: 8000,
+    livereload: true
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['templates/**'], ['pug']);
+  gulp.watch(['css/**'], ['stylus']);
+});
+
+gulp.task('build', [ 'pug', 'stylus' ]);
+gulp.task('serve', [ 'build', 'dev_server', 'watch' ]);
+gulp.task('default', [ 'build' ]);
